df281eaef7629890c4964cce82e54215d60af0f4
exersalza <squidodle44@gmail.com>
2022-12-02 09:32:10 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-window.onload = displayClock();
+data = [
+    {
+        name: "salty-cpp-bot",
+        desc: "This is my first Real cpp Project.",
+        url: "https://github.com/exersalza/salty-cpp-bot"
+    },
+    {
+        name: "kenexar-core",
+        desc: "This Project includes widely used Python snippets used by <a href='https://kenexar.eu/'>Kenexar</a>.",
+        url: "https://github.com/Kenexar/kenexar-core"
+    },
+]
 
 function displayClock() {
     let a;
@@ -8,4 +19,17 @@ function displayClock() {
         time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
         document.getElementById('clock').innerHTML = time;
     }, 1000);
+}
+
+function openPortfolio() {
+    let portfolio = $('.portfolio');
+
+    $('.portfolio-member').remove();
+
+    $('#1 > .cmd').html('ls -la .Portfolio/');
+
+    for (let i in data) {
+        portfolio.append(`<span class="portfolio-member">drwxr-xr-x ${i} julian exersalza 4096 
+                <a href="${data[i].url}">${data[i].name} :&nbsp;${data[i].desc}</a><br></span>`);
+    }
 }
